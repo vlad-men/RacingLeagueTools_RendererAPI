33c3aaaf8303d42be50476d2ba39bd64ebc04a25
vlad.men <vlad.men.mail@ya.ru>
2022-06-20 00:20:35 +1100

    add RenderType enum

---
+++ Base/RenderTyps.cs
@@ -0,0 +1,15 @@
+public enum RenderType
+{
+    RaceResults,
+    QualResults,
+    CombinedQualResults,
+    DriverStandings,
+    TeamStandings,
+    Lineups,
+    LineupsReserve,
+    PureRacePaceRating,
+    ConsistancyRating,
+    RaceTopFastestLaps,
+    RaceLongestStints,
+    Calendar
+}
