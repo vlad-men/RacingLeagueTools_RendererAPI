33fdebc599dacc7cfd5a84ae954a7f7eb8289058
vlad.men <vlad.men.mail@ya.ru>
2022-11-11 20:55:11 +1100

    Added GamePlatform enum.

---
+++ Base/GamePlatform.cs
@@ -0,0 +1,35 @@
+public enum GamePlatform
+    {
+        [Description("")]
+        Unspecified,
+        [Description("PC")]
+        Pc,
+        [Description("Playstation")]
+        Ps,
+        [Description("Xbox")]
+        Xbox,
+        [Description("Steam")]
+        PcSteam,
+        [Description("Origin")]
+        PcOrigin,
+        [Description("EGS")]
+        PcEgs,
+        [Description("Ubisoft")]
+        PcUbisoft,
+        [Description("PlayStation 4")]
+        Ps4,
+        [Description("PlayStation 5")]
+        Ps5,
+        [Description("Xbox One")]
+        XboxOne,
+        [Description("Xbox Series X|S")]
+        XboxSeriesXs,
+        [Description("Mac")]
+        Mac,
+        [Description("Android")]
+        Android,
+        [Description("iOS")]
+        Ios,
+        [Description("Nintendo Switch")]
+        Switch
+    }
